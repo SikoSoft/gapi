536c629f97afdeabd83ce06888198d46af3aab76
Aaron Wright <aaron@sikosoft.com>
2024-05-27 19:09:21 +0200

    refactor action endpoint to make code easier to follow

--- src/functions/action.ts
+++ src/functions/action.ts
@@ -7,7 +7,6 @@ import {
 import { jsonReply, prisma, userIdFromRequest } from "..";
 import { Action, Prisma } from "@prisma/client";
 import { Tagging } from "../lib/Tagging";
-import { v4 as uuidv4 } from "uuid";
 import {
   ListFilter,
   ListFilterTimeType,
@@ -16,14 +15,101 @@ import {
 
 const perPage = 25;
 
-const oneOfIsTags = ["food"];
-
 declare interface RequestBody {
   desc: string;
   type: string;
   occurredAt?: string;
   timeZone: string;
   tags: string[];
+}
+
+function getFilter(request: HttpRequest): ListFilter {
+  if (request.query.has("filter")) {
+    return JSON.parse(request.query.get("filter")) as ListFilter;
+  }
+
+  return {
+    tagging: {
+      [ListFilterType.CONTAINS_ONE_OF]: [],
+      [ListFilterType.CONTAINS_ALL_OF]: [],
+    },
+    time: {
+      type: ListFilterTimeType.ALL_TIME,
+    },
+    includeUntagged: true,
+    includeAll: true,
+  };
+}
+
+function getFilteredConditions(userId: string, filter: ListFilter) {
+  let startTime: Date;
+  let endTime: Date;
+  if (filter.time.type === ListFilterTimeType.EXACT_DATE) {
+    startTime = new Date(filter.time.date);
+    endTime = new Date(startTime.getTime() + 86400000);
+  }
+  if (filter.time.type === ListFilterTimeType.RANGE) {
+    startTime = new Date(filter.time.start);
+    endTime = new Date(new Date(filter.time.end).getTime() + 86400000);
+  }
+
+  return Prisma.validator(
+    prisma,
+    "action",
+    "findMany",
+    "where"
+  )({
+    userId,
+    ...(!filter.includeAll
+      ? {
+          AND: [
+            {
+              ...(filter.time.type === ListFilterTimeType.ALL_TIME
+                ? { occurredAt: { lte: new Date() } }
+                : {
+                    AND: [
+                      { occurredAt: { gte: startTime } },
+                      { occurredAt: { lte: endTime } },
+                    ],
+                  }),
+            },
+            {
+              OR: [
+                {
+                  ...(filter.includeUntagged ? { tags: { none: {} } } : {}),
+                },
+                {
+                  AND: [
+                    {
+                      ...(filter.tagging.containsOneOf.length
+                        ? {
+                            OR: [
+                              ...filter.tagging.containsOneOf.map((tag) => ({
+                                tags: { some: { label: tag } },
+                              })),
+                            ],
+                          }
+                        : {}),
+                    },
+                    {
+                      ...(filter.tagging.containsAllOf
+                        ? {
+                            AND: [
+                              ...filter.tagging.containsAllOf.map((tag) => ({
+                                tags: { some: { label: tag } },
+                              })),
+                            ],
+                          }
+                        : {}),
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        }
+      : {}),
+  });
 }
 
 export async function action(
@@ -76,112 +162,30 @@ export async function action(
       });
       return jsonReply({ action });
     case "GET":
-      let start = 0;
-      if (request.query.has("start")) {
-        start = parseInt(request.query.get("start") || "");
-      }
-      let filter: ListFilter = {
-        tagging: {
-          [ListFilterType.CONTAINS_ONE_OF]: [],
-          [ListFilterType.CONTAINS_ALL_OF]: [],
-        },
-        time: {
-          type: ListFilterTimeType.ALL_TIME,
-        },
-        includeUntagged: true,
-        includeAll: true,
-      };
-      if (request.query.has("filter")) {
-        filter = JSON.parse(request.query.get("filter")) as ListFilter;
-      }
-      console.log("###########", filter);
-      let startTime: Date;
-      let endTime: Date;
-      if (filter.time.type === ListFilterTimeType.EXACT_DATE) {
-        startTime = new Date(filter.time.date);
-        endTime = new Date(startTime.getTime() + 86400000);
-      }
-      if (filter.time.type === ListFilterTimeType.RANGE) {
-        startTime = new Date(filter.time.start);
-        endTime = new Date(new Date(filter.time.end).getTime() + 86400000);
-      }
+      const start = request.query.has("start")
+        ? parseInt(request.query.get("start") || "")
+        : 0;
+      const userId = userIdFromRequest(request);
+      const filter = getFilter(request);
+      const where = getFilteredConditions(userId, filter);
 
-      const where = Prisma.validator(
-        prisma,
-        "action",
-        "findMany",
-        "where"
-      )({
-        userId: userIdFromRequest(request),
-        ...(!filter.includeAll
-          ? {
-              AND: [
-                {
-                  ...(filter.time.type === ListFilterTimeType.ALL_TIME
-                    ? { occurredAt: { lte: new Date() } }
-                    : {
-                        AND: [
-                          { occurredAt: { gte: startTime } },
-                          { occurredAt: { lte: endTime } },
-                        ],
-                      }),
-                },
-                {
-                  OR: [
-                    {
-                      ...(filter.includeUntagged ? { tags: { none: {} } } : {}),
-                    },
-                    {
-                      AND: [
-                        {
-                          ...(filter.tagging.containsOneOf.length
-                            ? {
-                                OR: [
-                                  ...filter.tagging.containsOneOf.map(
-                                    (tag) => ({
-                                      tags: { some: { label: tag } },
-                                    })
-                                  ),
-                                ],
-                              }
-                            : {}),
-                        },
-                        {
-                          ...(filter.tagging.containsAllOf
-                            ? {
-                                AND: [
-                                  ...filter.tagging.containsAllOf.map(
-                                    (tag) => ({
-                                      tags: { some: { label: tag } },
-                                    })
-                                  ),
-                                ],
-                              }
-                            : {}),
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            }
-          : {}),
-      });
-      const rawActions = await prisma.action.findMany({
-        skip: start,
-        take: perPage,
-        where,
-        orderBy: {
-          occurredAt: "desc",
-        },
-        include: {
-          tags: true,
-        },
-      });
-      const actions = rawActions.map((action) => ({
+      const actions = (
+        await prisma.action.findMany({
+          skip: start,
+          take: perPage,
+          where,
+          orderBy: {
+            occurredAt: "desc",
+          },
+          include: {
+            tags: true,
+          },
+        })
+      ).map((action) => ({
         ...action,
         tags: action.tags.map((tag) => tag.label),
       }));
+
       const total = await prisma.action.count({ where });
       return jsonReply({
         time: new Date().toISOString(),

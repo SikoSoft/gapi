89c36ecec8cc50f8cccf1927cd8486426a4ff660
Aaron Wright <aaron@sikosoft.com>
2024-11-10 20:27:42 +0100

    add new endpoint listConfig with stub implementation

---
+++ src/functions/listConfig.ts
@@ -0,0 +1,32 @@
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
+import { getDefaultFilter, getDefaultSort, jsonReply } from "..";
+import { ListConfig } from "api-spec/models/List";
+
+const defaultListConfig: ListConfig = {
+  name: "Example config",
+  id: "test-id",
+  filter: getDefaultFilter(),
+  sort: getDefaultSort(),
+};
+
+export async function listConfig(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  switch (request.method) {
+    case "GET":
+      return jsonReply({ listConfigs: [defaultListConfig] });
+  }
+}
+
+app.http("listConfig", {
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  authLevel: "anonymous",
+  handler: listConfig,
+  route: "listConfig/{id?}",
+});

99536240e02bb97d3c4a9c3a259016556a0c5a98
Aaron Wright <aaron@sikosoft.com>
2025-01-28 18:40:19 +0100

    implement logic to save listConfig

--- src/functions/listConfig.ts
+++ src/functions/listConfig.ts
@@ -1,11 +1,23 @@
+import { v4 as uuidv4 } from "uuid";
 import {
   app,
   HttpRequest,
   HttpResponseInit,
   InvocationContext,
 } from "@azure/functions";
-import { getDefaultFilter, getDefaultSort, jsonReply } from "..";
-import { ListConfig } from "api-spec/models/List";
+import {
+  forbiddenReply,
+  getDefaultFilter,
+  getDefaultSort,
+  introspect,
+  jsonReply,
+  prisma,
+} from "..";
+import {
+  ListConfig,
+  ListSortDirection,
+  ListSortProperty,
+} from "api-spec/models/List";
 
 const defaultListConfig: ListConfig = {
   name: "Example config",
@@ -14,16 +26,56 @@ const defaultListConfig: ListConfig = {
   sort: getDefaultSort(),
 };
 
+export interface RequestBody {
+  id: string;
+  name: string;
+  userId: string;
+}
+
 export async function listConfig(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
+  const introspection = await introspect(request);
+  if (!introspection.isLoggedIn) {
+    return forbiddenReply();
+  }
+  const userId = introspection.user.id;
+
+  const body = (await request.json()) as RequestBody;
+
   switch (request.method) {
     case "GET":
       return jsonReply({ listConfigs: [defaultListConfig] });
     case "POST":
       let status = false;
-      return jsonReply({ status });
+      const id = uuidv4();
+      await prisma.listConfig.create({
+        data: {
+          id,
+          name: body.name,
+          userId,
+          filter: {
+            create: { includeAll: true, includeUntagged: true },
+          },
+          sort: {
+            create: {
+              property: ListSortProperty.CREATED_AT,
+              direction: ListSortDirection.DESC,
+            },
+          },
+        },
+      });
+      return jsonReply({ id });
+    case "PUT":
+      await prisma.listConfig.update({
+        data: { id: body.id, name: body.name },
+        where: {
+          id: body.id,
+          userId,
+        },
+      });
+      return jsonReply({ id: body.id });
   }
 }
 

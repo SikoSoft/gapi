9b044aee72eb8a03301bd1204c04f9add6b1abe5
Aaron Wright <aaron@sikosoft.com>
2025-03-10 17:07:47 +0100

    add introspect endpoint

---
+++ src/functions/introspect.ts
@@ -0,0 +1,36 @@
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
+import { IdentityManager } from "../lib/IdentityManager";
+import { forbiddenReply, introspect, jsonReply } from "..";
+
+declare interface RequestBody {
+  username: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+}
+
+export async function user(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
+
+  const introspection = await introspect(request);
+  if (!introspection.isLoggedIn) {
+    return forbiddenReply();
+  }
+
+  return jsonReply({ introspection });
+}
+
+app.http("user", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: user,
+  route: "user/introspect",
+});

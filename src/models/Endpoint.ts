a58110fd0c511a6b1454649b3290e292227567dc
Aaron Wright <aaron@sikosoft.com>
2025-03-20 17:28:40 +0100

    add Endpoint models file to define expected request & response body of endpoints

---
+++ src/models/Endpoint.ts
@@ -0,0 +1,31 @@
+import { ActionList } from "./Action";
+
+export enum HttpMethod {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+}
+
+export enum EndpointName {
+  ACTION = "action",
+  ACTION_SUGGESTION = "actionSuggestion",
+  INTROSPECT = "introspect",
+  LEADERBOARD = "leaderboard",
+  LIST_CONFIG = "listConfig",
+  LOGIN = "login",
+  LOGOUT = "logout",
+  OPERATION = "operation",
+  SETTING = "setting",
+  TAG = "tag",
+  TAG_SUGGESTION = "tagSuggestion",
+  USER = "user",
+}
+
+export interface EndpointConfig {
+  [EndpointName.ACTION]: {
+    [HttpMethod.GET]: {
+      responseBody: ActionList;
+    };
+  };
+}

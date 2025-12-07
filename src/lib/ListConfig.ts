823b59250e7618ca155f925439dc28f7ee6d921a
Aaron Wright <aaron@sikosoft.com>
2025-12-07 20:22:35 +0100

    update api-spec to 1.45.0 and update ListConfig logic to no longer map or save 'public' property

--- src/lib/ListConfig.ts
+++ src/lib/ListConfig.ts
@@ -92,7 +92,6 @@ export class ListConfig {
         data: {
           id: listConfig.id,
           name: listConfig.name,
-          public: listConfig.public,
         },
         where: {
           id: listConfig.id,
@@ -488,7 +487,6 @@ export class ListConfig {
       userId: data.userId,
       id: data.id,
       name: data.name,
-      public: data.public,
       filter: ListConfig.mapFilterDataToSpec(data.filter),
       sort: ListConfig.mapSortDataToSpec(data.sort),
       setting: ListConfig.mapSettingDataToSpec(data.setting),

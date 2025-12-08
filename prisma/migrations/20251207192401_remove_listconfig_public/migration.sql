ce2314048c183f116a6211f29b772f240cf886ee
Aaron Wright <aaron@sikosoft.com>
2025-12-08 18:23:18 +0100

    add prisma migration for removing 'public' from ListConfig

---
+++ prisma/migrations/20251207192401_remove_listconfig_public/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `public` on the `ListConfig` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ListConfig" DROP COLUMN "public";

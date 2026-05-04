873e3a478ba3b5db0387b6a3ef63aaa60e4d516e
Aaron Wright <aaron@sikosoft.com>
2026-05-04 21:07:50 +0200

    update tsup.config and workflow again

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["./src/**/*.ts"],
   format: "esm",
+  platform: "node",
   noExternal: [/^(?!(argon2|@prisma\/client|@azure\/functions))/],
   clean: true,
 });

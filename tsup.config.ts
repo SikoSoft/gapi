70bb034aa5f74fc8eff959f536988628f05dde12
Aaron Wright <aaron@sikosoft.com>
2026-05-05 08:36:04 +0200

    update tsup config so that createRequire is injected via banner config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "tsup";
+import { builtinModules } from "module";
+
+const externals = [
+  "argon2",
+  "@prisma/client",
+  "@azure/functions",
+  "node:",
+  ...builtinModules.filter((m) => !m.startsWith("_")),
+];
 
 export default defineConfig({
   entry: ["./src/**/*.ts"],
   format: "esm",
   platform: "node",
-  noExternal: [/^(?!(argon2|@prisma\/client|@azure\/functions))/],
+  noExternal: [new RegExp(`^(?!(${externals.join("|")}))`)],
+  banner: {
+    js: `import { createRequire } from 'module'; const require = createRequire(import.meta.url);`,
+  },
   clean: true,
 });

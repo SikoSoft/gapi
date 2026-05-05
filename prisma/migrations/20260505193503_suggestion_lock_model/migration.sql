ee7c47cc5ba25696a8fd801a966482c22dffd636
Aaron Wright <aaron@sikosoft.com>
2026-05-05 21:35:49 +0200

    add SuggestionLock model, new migration

---
+++ prisma/migrations/20260505193503_suggestion_lock_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "SuggestionLock" (
+    "yyyy" INTEGER NOT NULL,
+    "mm" INTEGER NOT NULL,
+    "dd" INTEGER NOT NULL,
+
+    CONSTRAINT "SuggestionLock_pkey" PRIMARY KEY ("yyyy","mm","dd")
+);
